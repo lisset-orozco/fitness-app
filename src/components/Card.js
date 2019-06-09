f0b8616e139c7bfabdb6c6393040a8a8ebfeb842
lisset-orozco <liss612@gmail.com>
2019-06-09 00:13:37 -0500

    component and props

---
+++ src/components/Card.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './styles/Card.css';
+import exerciseImg from '../images/exercise.png'; 
+import circlesImg from '../images/circles.png'; 
+
+class Card extends React.Component {
+  // background: url('../../images/circles.png') no-repeat, linear-gradient(to right, #A74CF2, #617BFB);
+  render() {
+    const { title, description, img, leftColor, rightColor} = this.props
+    return (
+      <div className="card mx-auto Fitness-Card"
+      style={{
+        backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor}, ${rightColor})`
+      }}
+      >
+        <div className="card-body">
+          <div className="row center">
+            <div className="col-6">
+              <img src={img} alt='exercise' className="float-right"/>
+            </div>
+            <div className="col-6 Fitness-Card-Info">
+              <h1>{title}</h1>
+              <p>{description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  } 
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'bootstrap/dist/css/bootstrap.css';
+import Card from './components/Card';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const container = document.getElementById('root');
+
+ReactDOM.render(<Card
+                    title='Technique Guides'
+                    description='Learn amazing street workout and calisthenics'
+                    img='https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06'
+                    leftColor='#A74CF2'
+                    rightColor='#617BFB'
+                />, container);
