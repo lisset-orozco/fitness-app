97b73ef005b86da9d378d69e7e8249f3f3b52942
lisset-orozco <liss612@gmail.com>
2019-06-09 14:50:49 -0500

    dynamic routing of components

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import './styles/Add.css';
 import btnAdd from '../images/add.png';
 
 const Add = () => (
-  <div>
+  <Link to="/exercise/new">
     <img src={btnAdd} alt='add' className="Fitness-Add" />
-  </div>
+  </Link>
 )
 
 export default Add;
---
+++ src/pages/ExerciseNew.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ExerciseNew = () => (
+  'ddd'
+)
+
+export default ExerciseNew;
