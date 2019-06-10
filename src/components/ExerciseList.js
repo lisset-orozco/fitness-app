60e9966d45238f59db2a9299f08963505794738f
lisset-orozco <liss612@gmail.com>
2019-06-09 23:28:45 -0500

    requesting data to the api

--- src/components/ExerciseList.js
+++ src/components/ExerciseList.js
@@ -5,16 +5,11 @@ import Card from '../components/Card';
 const ExerciseList = ({Data}) => (
   <div>
     {
-      Data.map((exercise, index) => (
+      Data.map((exercise, index) =>
         <Card
-          title={exercise.title}
-          description={exercise.description}
-          img={exercise.img}
-          leftColor={exercise.leftColor}
-          rightColor={exercise.rightColor}
+          {...exercise}
           key={index}
         />
-        )
       )
     }
   </div>
--- src/pages/Exercises.js
+++ src/pages/Exercises.js
@@ -8,14 +8,38 @@ import Add from '../components/Add';
 import Data from '../faker/exercises.json'
 
 class Exercises extends React.Component {
-  state = { Data };
+  state = { Data: [], hasError: false };
+
+  async componentDidMount() {
+    
+    try {
+        await this.fetchExercises() 
+      } catch (e) {
+        console.log('Error')
+        this.setState({ hasError: true });
+      }
+  }
+
+  fetchExercises = async () => {
+    let res = await fetch('http://localhost:8000/api/exercises');
+    let data = await res.json();
+
+    this.setState({
+      Data: data
+    })
+    console.log(data)
+  }
 
   render = () => (
-    <div>
-      <Welcome username='Alexandra' />
-      <ExercisesList Data={this.state.Data}/>
-      <Add />
-    </div>
+    this.state.hasError 
+    ?
+      <h1>Something went wrong.</h1>
+    :
+      <div>
+        <Welcome username='Alexandra' />
+        <ExercisesList Data={this.state.Data}/>
+        <Add />
+      </div>
   )
 }
 
