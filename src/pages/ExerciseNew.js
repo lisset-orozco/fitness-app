d4ab27bf47729a11b5874bbdeb605f7d40259bd8
lisset-orozco <liss612@gmail.com>
2019-06-09 21:37:22 -0500

    form, event and state

--- src/pages/ExerciseNew.js
+++ src/pages/ExerciseNew.js
@@ -1,11 +1,93 @@
 import React from 'react';
 
-const handleClick = () => console.log('Clicked');
+class ExerciseNew extends React.Component {
+  state = {
+    title: '',
+    description: '',
+    img: '',
+    leftColor: '',
+    rightColor: ''
+  }
 
-const ExerciseNew = () => (
-  <button onClick={ () => handleClick() }>
-    Send
-  </button>
-);
+  handleChange = e => {
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log(this.state)
+  }
+
+  render(){
+    return (
+        <div className="container">
+        <form onSubmit={this.handleSubmit} >
+          <div className="form-group">
+            <input 
+              type="text" 
+              className="form-control" 
+              placeholder="title" 
+              name="title"
+              onChange={this.handleChange}
+              value={this.state.title}
+            />
+          </div>
+          <div className="form-group">
+            <input 
+              type="text" 
+              className="form-control" 
+              placeholder="description" 
+              name="description"
+              onChange={this.handleChange}
+              value={this.state.description}
+            />
+          </div>
+          <div className="form-group">
+            <input 
+              type="text" 
+              className="form-control" 
+              placeholder="img" 
+              name="img"
+              onChange={this.handleChange}
+              value={this.state.img}
+            />
+          </div>
+          <div className="form-row">
+            <div className="col">
+              <input 
+                type="text" 
+                className="form-control" 
+                placeholder="leftColor" 
+                name="leftColor"
+                onChange={this.handleChange}
+                value={this.state.leftColor}
+              />
+            </div>
+            <div className="col">
+              <input 
+                type="text" 
+                className="form-control"
+                placeholder="rightColor" 
+                name="rightColor"
+                onChange={this.handleChange}
+                value={this.state.rightColor}
+              />    
+            </div>
+          </div>
+          
+          <button 
+            type="submit" 
+            className="btn btn-primary"
+          >
+            Submit
+          </button>
+        </form>
+    </div>
+    )
+}
+
+}
 
 export default ExerciseNew;
