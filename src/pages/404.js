ef84e08dbb581c3b177eb46f937e549abf24f7e5
lisset-orozco <liss612@gmail.com>
2019-07-12 01:57:33 -0500

    fix errors to responsive

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import '../components/styles/Errors.css';
 import NotFoundImg from '../images/404.png';
+import { Grid , Container} from '@material-ui/core';
 
 const NotFound = () => (
-  <div className="text-center">
-    <h1 className="Error_Text">Error: 404 Page Not Found</h1>    
-    <img src={NotFoundImg} alt="404 NotFound" className="Error_Image" />
+  <div align="center" className="Error_Margin">
+    <b className="Error_Text"><span>Error: 404</span><br/>
+    <span>Page Not Found</span></b>
+    <img src={NotFoundImg} alt="404 NotFound" className="Error" /> 
   </div>
 )
 
--- src/pages/500.js
+++ src/pages/500.js
@@ -4,9 +4,10 @@ import '../components/styles/Errors.css';
 import FatalErrorImg from '../images/500.png';
 
 const FatalError = () => (
-  <div className="text-center">
-    <h1 className="Error_Text">Error: 500 Unexpected Error</h1>    
-    <img src={FatalErrorImg} alt="500 Unexpected Error" className="Error_Image" />
+  <div align="center" className="Error_Margin">
+    <b className="Error_Text"><span>Error: 500</span><br/>
+    <span>Unexpected Error</span></b>
+    <img src={FatalErrorImg} alt="500 Unexpected Error" className="Error" /> 
   </div>
 )
 
